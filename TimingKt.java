98630ffc818f34737d2126f0dd14b381f6b391da
kongali1720 <121486486+kongali1720@users.noreply.github.com>
2026-07-17 04:26:39 +0700

    Implement timing functions for execution measurement
    Added functions to measure execution time in milliseconds and nanoseconds.

---
+++ TimingKt.java
@@ -0,0 +1,23 @@
+package kotlin.system;
+
+import kotlin.Metadata;
+import kotlin.jvm.functions.Function0;
+import kotlin.jvm.internal.Intrinsics;
+
+@Metadata(d1 = {"\u0000\u0014\n\u0000\n\u0002\u0010\t\n\u0000\n\u0002\u0018\u0002\n\u0002\u0010\u0002\n\u0002\b\u0002\u001a'\u0010\u0000\u001a\u00020\u00012\f\u0010\u0002\u001a\b\u0012\u0004\u0012\u00020\u00040\u0003H\u0086\bÃ¸\u0001\u0000\u0082\u0002\n\n\b\b\u0001\u0012\u0002\u0010\u0001 \u0001\u001a'\u0010\u0005\u001a\u00020\u00012\f\u0010\u0002\u001a\b\u0012\u0004\u0012\u00020\u00040\u0003H\u0086\bÃ¸\u0001\u0000\u0082\u0002\n\n\b\b\u0001\u0012\u0002\u0010\u0001 \u0001\u0082\u0002\u0007\n\u0005\b\u009920\u0001Â¨\u0006\u0006"}, d2 = {"measureNanoTime", "", "block", "Lkotlin/Function0;", "", "measureTimeMillis", "kotlin-stdlib"}, k = 2, mv = {1, 9, 0}, xi = 48)
+/* loaded from: /storage/emulated/0/Android/data/com.apktools.app.decompile/files/decompile_temp/jadx/classes12.dex */
+public final class TimingKt {
+    public static final long measureTimeMillis(Function0 function0) {
+        Intrinsics.checkNotNullParameter(function0, "");
+        long currentTimeMillis = System.currentTimeMillis();
+        function0.invoke();
+        return System.currentTimeMillis() - currentTimeMillis;
+    }
+
+    public static final long measureNanoTime(Function0 function0) {
+        Intrinsics.checkNotNullParameter(function0, "");
+        long nanoTime = System.nanoTime();
+        function0.invoke();
+        return System.nanoTime() - nanoTime;
+    }
+}
